84dbd28db1d632cab858169de9dbdc013487816a
012balaji <144882695+012balaji@users.noreply.github.com>
2026-04-13 17:25:16 +0530

    remove node

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { registerUser } from "./api";
+import { registerUser } from "../api";
 
 function LoginPage() {
   const [formData, setFormData] = useState({
